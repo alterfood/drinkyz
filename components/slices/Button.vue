8a67c0a566eb5797e2642a0f5abeeafae8fbfa45
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-04-23 16:43:59 +0200

    add button component

---
+++ components/slices/Button.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { getSliceComponentProps } from "@prismicio/vue"
+
+defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
+</script>
+<template>
+    <div class="flex justify-center my-12">
+        <a href="" class="border-2 border-black px-6 py-3">
+            <PrismicRichText :field="slice.primary.label" class="text-2xl font-bold" />
+        </a>
+    </div>
+</template>
