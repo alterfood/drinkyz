e5dbf29d6a03dfb453a797671114da56fddc4095
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-04-24 11:48:48 +0200

    header

--- components/header/Index.vue
+++ components/header/Index.vue
@@ -1,53 +1,21 @@
 <script setup lang="ts">import { usePrismicDocuments } from '@prismicio/vue';
-
-    // const { client } = usePrismic()
-    // const { data: home } = await useAsyncData('home', () => useFirstPrismicDocument())
-    // console.log(home)
-
-    // const { data: documents, state, error, refresh } = usePrismicDocuments();
-    const items = reactive([
-        {
-            title: 'Services',
-            slug: 'boissons'
-        },
-        {
-            title: 'Eau',
-            slug: 'bouteilles'
-        },
-        {
-            title: 'Soft',
-            slug: 'jus-de-fruits'
-        },
-        {
-            title: 'Jus',
-            slug: 'soda'
-        },
-        {
-            title: 'Soupe',
-            slug: 'soda'
-        },
-        {
-            title: 'Alterfood',
-            slug: 'soda'
-        },
-        {
-            title: 'Contact',
-            slug: 'soda'
-        }
-    ])
+    const { client } = usePrismic()
+    const { data: menu } = await useAsyncData('home', () => client.getByID('YmUadxMAACUAYQFr'))
 </script>
 <template>
     <header class="flex justify-between text-gray-100 bg-black font-bold">
         <div></div>
         <div class="flex">
-            <img src="logo" alt="Drinkyz.com" class="mr-10 my-2 h-12" />
+            <!-- <img src="logo" alt="Drinkyz.com" class="mr-10 my-2 h-12" /> -->
             <ul class="flex  py-5">
                 <li 
-                    v-for="(item, index) in items"
+                    v-for="(menuLink, index) in menu.data.menu_links"
                     :key="index"
                     class="mx-4"
                 >
-                    {{ item.title }}
+                    <NuxtLink :to="'/' + menuLink.link.uid">
+                        <PrismicRichText :field="menuLink.label" />    
+                    </NuxtLink>
                 </li>
             </ul>
         </div>
