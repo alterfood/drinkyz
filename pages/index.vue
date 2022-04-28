1851c7e4f374bedf36126c54ed82d84bf981e4d1
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-04-28 09:11:37 +0200

    add favicon meta

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,25 @@ const components = defineSliceZoneComponents({
     'page_squares': LazySlicesPageSquares,
     'page_brand': LazySlicesPageBrands,
 });
+
+useHead({
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: 'My amazing site.' },
+    { name: 'msapplication-TileColor', content: '#da532c' },
+    { name: 'theme-color', content: '#ffffff'}
+  ],
+  link: [
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
+    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+  ],
+
+})
+
 </script>
 <template>
     <Head>
