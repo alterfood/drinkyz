3e3cf295ee63c33ee7f5bfba510af584008e4fbb
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-04-28 10:07:40 +0200

    align button code

--- components/slices/Button.vue
+++ components/slices/Button.vue
@@ -6,17 +6,17 @@ defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
 <template>
     <div class="flex justify-center my-12">
         <NuxtLink 
-            v-if="slice.primary.link.uid" :to="'/' + slice.primary.link.uid"
+            v-if="slice.primary.link.uid" :to="'/' + slice.primary.button_link.uid"
             class="border-2 border-black px-6 py-3"
         >
-            <PrismicRichText :field="slice.primary.label" class="text-2xl font-bold" />
+            <PrismicRichText :field="slice.primary.button_label" class="text-2xl font-bold" />
         </NuxtLink>
         <NuxtLink v-else 
-            :to="slice.primary.link.url"
+            :to="slice.primary.button_link.url"
             target="_blank"
             class="border-2 border-black px-6 py-3"
         >
-            <PrismicRichText :field="slice.primary.link.label" />    
+            <PrismicRichText :field="slice.primary.button_label" />    
         </NuxtLink>
     </div>
 </template>
