f9f3466f0428e607be740836b01534df44a20ec6
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-05-13 18:50:50 +0200

    Image component

--- components/slices/PageImage.vue
+++ components/slices/PageImage.vue
@@ -4,10 +4,7 @@ import { getSliceComponentProps } from "@prismicio/vue"
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
 </script>
 <template>
-    <template
-        v-for="(item, index) in slice.items"
-        :key="index"
-    >
-        <PrismicImage :field="item.image" />
-    </template>
+<div class="flex justify-center">
+    <PrismicImage :field="slice.primary.image" />
+</div>
 </template>
