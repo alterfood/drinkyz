fe2619e079af0962141a04960aeee7c1a48f9f53
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-05-14 12:18:10 +0200

    fix square

--- components/slices/PageSquare.vue
+++ components/slices/PageSquare.vue
@@ -4,17 +4,19 @@ import { getSliceComponentProps } from "@prismicio/vue"
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
 </script>
 <template>
-    <NuxtLink 
-        :to="{ name: 'page', params: { page: slice.square_link.uid } }"
-    >
-        <div class="bg-gray-200 grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <div class="">
-                <PrismicImage :field="slice.square_image" />
+    <section class="my-6">
+        <NuxtLink 
+            :to="{ name: 'page', params: { page: slice.primary.square_link.uid } }"
+        >
+            <div class="p-4 bg-gray-200 flex gap-6">
+                <div class="">
+                    <PrismicImage :field="slice.primary.square_image" />
+                </div>
+                <div class="flex-col justify-center p-5">
+                    <PrismicRichText :field="slice.primary.square_title" class="text-3xl font-extrabold" />
+                    <PrismicRichText :field="slice.primary.square_text" />
+                </div>
             </div>
-            <div class="flex justify-center text-center  p-5">
-                <PrismicRichText :field="slice.square_title" class="text-3xl font-extrabold" />
-                <PrismicRichText :field="slice.square_text" />
-            </div>
-        </div>
-    </NuxtLink>
+        </NuxtLink>
+    </section>
 </template>
--- components/slices/PageSquares.vue
+++ components/slices/PageSquares.vue
@@ -4,19 +4,21 @@ import { getSliceComponentProps } from "@prismicio/vue"
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
 </script>
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
-        <template
-            v-for="(item, index) in slice.items"
-            :key="index"
-        >
-            <NuxtLink 
-                :to="{ name: 'page', params: { page: item.square_link.uid } }"
+    <section class="my-6">
+        <div class="grid grid-cols-1 sm:grid-cols-2 gap-6">
+            <template
+                v-for="(item, index) in slice.items"
+                :key="index"
             >
-                <div class="flex flex-col justify-center text-center bg-gray-200 p-5">
-                    <PrismicRichText :field="item.square_title" class="text-3xl font-extrabold" />
-                    <PrismicImage :field="item.square_image" />
-                </div>
-            </NuxtLink>
-        </template>
-    </div>
+                <NuxtLink 
+                    :to="{ name: 'page', params: { page: item.square_link.uid } }"
+                >
+                    <div class="flex flex-col justify-center text-center bg-gray-200 p-5">
+                        <PrismicRichText :field="item.square_title" class="text-3xl font-extrabold" />
+                        <PrismicImage :field="item.square_image" />
+                    </div>
+                </NuxtLink>
+            </template>
+        </div>
+    </section>
 </template>
