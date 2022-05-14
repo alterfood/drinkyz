9ac00606639a4af392fc06430ed8df061b7cd168
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-05-14 12:54:22 +0200

    brands design

--- components/slices/PageBrands.vue
+++ components/slices/PageBrands.vue
@@ -4,14 +4,16 @@ import { getSliceComponentProps } from "@prismicio/vue"
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
 </script>
 <template>
-    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-2">
-        <template
-            v-for="(item, index) in slice.items"
-            :key="index"
-        >
-            <div class="bg-black">
-                <PrismicImage :field="item.brand_image" />
-            </div>
-        </template>
-    </div>
+    <section class="mb-10">
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-2">
+            <template
+                v-for="(item, index) in slice.items"
+                :key="index"
+            >
+                <div class="bg-black p-5 flex justify-center content-center">
+                    <PrismicImage :field="item.brand_image" class="object-contain" />
+                </div>
+            </template>
+        </div>
+    </section>
 </template>
