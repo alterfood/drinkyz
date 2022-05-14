40cdf8fbed448207667119c74a906a367c412b8c
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-05-14 13:28:01 +0200

    square - responsive

--- components/slices/PageSquare.vue
+++ components/slices/PageSquare.vue
@@ -10,9 +10,10 @@ defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
         >
             <div class="p-4 bg-gray-200 flex gap-6">
                 <div class="">
+                    <PrismicRichText :field="slice.primary.square_title" class="text-3xl font-extrabold sm:hidden" />
                     <PrismicImage :field="slice.primary.square_image" />
                 </div>
-                <div class="flex-col justify-center p-5">
+                <div class="flex-col justify-center p-5 hidden sm:block">
                     <PrismicRichText :field="slice.primary.square_title" class="text-3xl font-extrabold" />
                     <PrismicRichText :field="slice.primary.square_text" />
                 </div>
