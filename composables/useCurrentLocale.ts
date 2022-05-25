1105349f6af867280cf9626387e586a5d548d2ed
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-05-25 08:02:51 +0200

    default currentLocale

--- composables/useCurrentLocale.ts
+++ composables/useCurrentLocale.ts
@@ -1,5 +1,5 @@
 export const useCurrentLocale = () => {
     const route = useRoute()
     
-    return (route.fullPath as string).substring(1, 6) || 'fr'
+    return (route.fullPath as string).substring(1, 6) || 'fr-fr'
 }
