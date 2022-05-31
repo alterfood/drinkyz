6618b0355897be7a642e5729b4e1b483c0e9486c
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-05-31 15:29:37 +0200

    fix button link

--- components/slices/Button.vue
+++ components/slices/Button.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { getSliceComponentProps } from "@prismicio/vue"
 
+const currentLocale = useCurrentLocale()
+
+
 defineProps(getSliceComponentProps(["slice", "index", "slices", "context"]))
 </script>
 <template>
     <div class="flex justify-center my-12">
         <NuxtLink 
             v-if="slice.primary.button_link.uid" 
-            :to="'/' + slice.primary.button_link.uid"
+            :to="`/${currentLocale}/${slice.primary.button_link.uid}`"
             class="border-2 border-black px-6 py-3"
         >
             <PrismicText :field="slice.primary.button_label" class="text-2xl font-bold" />
