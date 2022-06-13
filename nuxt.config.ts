80f77252d756482763d1fb0905eda5dd5bb92575
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-06-13 18:41:57 +0200

    add contact-success page to generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    nitro: {
+        prerender: {
+          routes: ['/fr-fr/contact-success', '/en-us/contact-success']
+        }
+    },
     typescript: {
         shim: false
     },
