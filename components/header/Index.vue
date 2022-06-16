f7fb050fd024abc236e865ff4d9f749d4ad537f9
Angelo Maiorano <angelo.maiorano@gmail.com>
2022-06-16 07:45:16 +0200

    submenus

--- components/header/Index.vue
+++ components/header/Index.vue
@@ -18,16 +18,33 @@ const locales = ref(useLocales())
 const { client } = usePrismic()
 const { data: menu, refresh } = await useAsyncData('menuHeader', () => client.getByID('YmUadxMAACUAYQFr'))
 
+for (const item of menu.value.data.menu_links) {
+    if(item.submenu.id) {
+        const { data: submenu, refresh } = await useAsyncData(item.submenu.id, () => client.getByID(item.submenu.id))
+        item.submenu = submenu.value.data.menu_links
+    }
+}
+
 watch(
   () => [route.fullPath],
   async() => {
     refresh
   },
 )
+
+const submenu = ref<string>('')
+
+const showSubmenu = (value: string) => {
+    submenu.value = value
+}
+
+const hideSubmenu = () => {
+    submenu.value = ''
+}
 </script>
 <template>
     <header 
-        class="flex justify-between text-gray-100 font-bold px-4 md:px-0"
+        class="flex justify-between text-gray-100 font-bold px-4 md:px-0 sm:h-16"
         :class="[bgColor === '#000000' ? 'bg-gradient-to-b from-black to-menub' : 'color']"
     >
         <div></div>
@@ -35,11 +52,13 @@ watch(
             <NuxtLink to="/" class="flex items-center">
                 <img src="/logo.svg" alt="Drinkyz.com" class="mr-10 my-2 h-12" />
             </NuxtLink>
-            <ul class="flex flex-wrap py-2 sm:py-5 mb-0 marker:text-transparent">
+            <ul class="flex flex-wrap mb-0 marker:text-transparent relative">
                 <li 
                     v-for="(menuLink, index) in menu.data.menu_links"
                     :key="index"
-                    class="mx-4 my-2 sm:my-0"
+                    class="mx-4 my-2 sm:my-5"
+                    @mouseover="showSubmenu(menuLink.submenu.id)"
+                    @mouseleave="hideSubmenu()"
                 >
                     <NuxtLink
                         v-if="menuLink.link.uid" 
@@ -50,6 +69,27 @@ watch(
                     <NuxtLink v-else :to="menuLink.link.url" target="_blank">
                         <PrismicText :field="menuLink.label" />    
                     </NuxtLink>
+                    <ul 
+                        v-if="menuLink.submenu.length"
+                        class="absolute bg-black -mx-2"
+                        v-show="submenu === menuLink.submenu.id"
+                    >
+                        <li 
+                            v-for="(submenuLink, index) in menuLink.submenu"
+                            :key="index"
+                            class="mx-4 my-4"
+                        >
+                            <NuxtLink
+                                v-if="submenuLink.link.uid" 
+                                :to="`/${currentLocale}/${submenuLink.link.uid}`"
+                            >
+                                <PrismicText :field="submenuLink.label" />    
+                            </NuxtLink>
+                            <NuxtLink v-else :to="submenuLink.link.url" target="_blank">                
+                                <PrismicText :field="submenuLink.label" />    
+                            </NuxtLink>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
